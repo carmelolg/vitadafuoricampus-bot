0972e45807100c8c32da25e112dfc85bc10c6b2e
carmelolg <carmelolagamba@hotmail.it>
2018-02-03 12:58:10 +0100

    Pages fixed

--- constants.js
+++ constants.js
@@ -1,2 +1,3 @@
 module.exports = {}
 module.exports.ERROR_MESSAGE_DEFAULT = 'Qualcosa è andato storto, ripova per favore.'
+module.exports.LAST_ITEM_NUMBER = 3
--- posts/index.js
+++ posts/index.js
@@ -1,19 +1,38 @@
 const Telegram = require('telegram-node-bot')
 const TelegramBaseController = Telegram.TelegramBaseController
+const Constant = require('../constants')
+const wpapi = require('./../wpapi.js').wpapi
+const dateFormat = require('dateformat');
 
-const wpapi = require('./../wpapi.js').wpapi;
 class PostsController extends TelegramBaseController {
 
     /**
-     * @param {Scope} $
+     * @param {Scope} scope
      */
-    handle($) {
-        $.sendMessage('Work in progress')
-
+    handle(scope) {
         wpapi.then(function (site) {
             site.posts().get(function (err, data) {
-                if (data)
-                    console.log(data)
+                if (data && data.length) {
+                    var posts = [];
+                    data.slice(0, Constant.LAST_ITEM_NUMBER).forEach(function (post) {
+                        posts.push({
+                            title: post.title.rendered,
+                            // abstract: post.excerpt.rendered,
+                            date: post.date,
+                            url: post.link
+                        });
+                    });
+
+                    posts.sort(function (a, b) {
+                        return a.date > b.date;
+                    });
+
+                    posts.forEach(function (post) {
+                        var message = post.title + '\nData: ' + dateFormat(post.date, 'dddd, mmmm dS, yyyy') + '\n\nClicca qui: ' + post.url;
+                        scope.sendMessage(message, {'parse_mode': 'Markdown'});
+                    })
+
+                }
 
                 if (err)
                     console.log(err)
